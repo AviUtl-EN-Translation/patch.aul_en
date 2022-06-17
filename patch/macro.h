0bfedbcf22081f5eecac07fb628acd6555446776
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-06-17 19:12:19 +0900

    ディスプレイスメントマップの追加

--- patch/macro.h
+++ patch/macro.h
@@ -94,7 +94,7 @@
 #endif //define PATCH_SWITCH_FAST
 
 #else // ifdef PATCH_INTERNAL
-#define PATCH_VERSION_STR "r40"
+#define PATCH_VERSION_STR "r40_test1"
 
 #define PATCH_SWITCH_EXCEPTION_LOG
 #define PATCH_SWITCH_SYSINFO_MODIFY
@@ -152,6 +152,7 @@
 	#define PATCH_SWITCH_CL cl
 	#ifdef PATCH_SWITCH_CL
 		#define PATCH_SWITCH_FAST_POLORTRANSFORM fast_polortransform
+		#define PATCH_SWITCH_FAST_DISPLACEMENTMAP fast_displacementmap
 		#define PATCH_SWITCH_FAST_RADIATIONALBLUR fast_radiationalblur
 		#define PATCH_SWITCH_FAST_FLASH fast_flash
 		#define PATCH_SWITCH_FAST_DIRECTIONALBLUR fast_directionalblur
