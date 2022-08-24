0bf730dbb929db661c6e8a9cf67d96276590e477
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-08-24 10:18:28 +0900

    Update patch_add_extension.hpp

--- patch/patch_add_extension.hpp
+++ patch/patch_add_extension.hpp
@@ -33,8 +33,7 @@ namespace patch {
     // 動画、音声ファイル参照の時の拡張子にexedit.iniにあるものを追加する機能
     inline class add_extension_t {
 
-        static int __cdecl wsprintfA_wrap2701a(int ebp, char* dst, char* src);
-
+        
         bool enabled = true;
         bool enabled_i;
 
@@ -42,6 +41,8 @@ namespace patch {
 
 
     public:
+static int __cdecl wsprintfA_wrap2701a(int ebp, char* dst, char* src);
+
 
 
         void init() {
